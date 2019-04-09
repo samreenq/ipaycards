8f8b8f24cdffe090f67cd31fc9a4e0f78f6e044c
ahsanzafar <ahsan.zafar@cubixlabs.com>
2019-04-09 12:25:20 +0500

    game slider done...

--- resources/views/web/includes/foot.blade.php
+++ resources/views/web/includes/foot.blade.php
@@ -32,7 +32,24 @@
 
 	$(document).ajaxComplete(function () {
         $('.brands_slide').owlCarousel({
-            loop:true,
+            loop:false,
+            slideBy: 1,
+            margin:10,
+            nav: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:4
+                }
+            }
+        });
+        $('.game_slide').owlCarousel({
+            loop:false,
             slideBy: 1,
             margin:10,
             nav: true,
