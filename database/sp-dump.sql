54db14f287538ea9e0461ab7c681a70ec18a5ce5
samreen <samreen.quyyum@cubixlabs.com>
2019-07-16 15:24:23 +0500

    update DB dump

--- database/sp-dump.sql
+++ database/sp-dump.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.09 (64 bit)
-MySQL - 10.1.34-MariaDB : Database - ipaycards_db
+MySQL - 5.7.26-0ubuntu0.16.04.1 : Database - ipaycards_db
 *********************************************************************
 */
 
@@ -12,84 +12,9 @@ MySQL - 10.1.34-MariaDB : Database - ipaycards_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/* Procedure structure for procedure `delete_entity_attribute_values` */
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ipaycards_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-/*!50003 DROP PROCEDURE IF EXISTS  `delete_entity_attribute_values` */;
-
-DELIMITER $$
-
-/*!50003 CREATE PROCEDURE `delete_entity_attribute_values`(IN _entity_id INT,IN _attribute_id INT , IN _table VARCHAR(100) )
-BEGIN
-	SET @stmt = CONCAT("DELETE FROM ",_table," WHERE entity_id = ", _entity_id," AND attribute_id = ", _attribute_id );
-	PREPARE statement FROM @stmt;
-	EXECUTE statement; 
-END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `get_entities_listing` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `get_entities_listing` */;
-
-DELIMITER $$
-
-/*!50003 CREATE PROCEDURE `get_entities_listing`(IN _entity_type_id INT, IN _attribute_code VARCHAR(250))
-BEGIN
-	DECLARE _attribute_id INT(20);
-	DECLARE _data_type VARCHAR(250);
-	
-	SELECT attribute_id, sys_data_type.type INTO _attribute_id, _data_type FROM sys_attribute 
-LEFT JOIN sys_data_type ON sys_data_type.data_type_id = sys_attribute.data_type_id
-WHERE attribute_code = _attribute_code;
-	
-	SET @stmt = CONCAT("SELECT entity_id,value FROM sys_entity_", _data_type, " WHERE entity_type_id = ", _entity_type_id, ' AND deleted_at is NULL', " AND  attribute_id = ",_attribute_id );
-	
-	#select @stmt;
-	PREPARE statement FROM @stmt;
-	EXECUTE statement;
-    END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `get_item_inventory` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `get_item_inventory` */;
-
-DELIMITER $$
-
-/*!50003 CREATE PROCEDURE `get_item_inventory`(IN _entity_id INT, IN _attribute_code VARCHAR(250))
-BEGIN
-	DECLARE _attribute_id INT(20);
-	DECLARE _data_type VARCHAR(250);
-	
-	SELECT attribute_id, sys_data_type.type INTO _attribute_id, _data_type FROM sys_attribute 
-LEFT JOIN sys_data_type ON sys_data_type.data_type_id = sys_attribute.data_type_id
-WHERE attribute_code = _attribute_code;
-	
-	SET @stmt = CONCAT("SELECT value FROM sys_entity_", _data_type, " WHERE entity_id = ", _entity_id, 
-	" AND  attribute_id = ",_attribute_id);
-	
-	#select @stmt;
-	PREPARE statement FROM @stmt;
-	EXECUTE statement;
-    END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `soft_delete_entity_data` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `soft_delete_entity_data` */;
-
-DELIMITER $$
-
-/*!50003 CREATE PROCEDURE `soft_delete_entity_data`( IN _entity_id INT(10), IN _deleted_at DATETIME)
-BEGIN
-	
-	UPDATE `sys_entity_int` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id;
-	UPDATE `sys_entity_varchar`  SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
-	UPDATE `sys_entity_decimal` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
-	UPDATE `sys_entity_text` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
-	UPDATE `sys_entity_time` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
-	UPDATE `sys_entity_datetime` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ; 
-END */$$
-DELIMITER ;
+USE `ipaycards_db`;
 
 /* Procedure structure for procedure `category_parents` */
 
@@ -97,7 +22,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `category_parents`(IN _category_id INT)
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `category_parents`(IN _category_id INT)
 BEGIN
 	DECLARE _parent_id INT;
 	DECLARE rv VARCHAR(100);
@@ -125,13 +50,27 @@ BEGIN
 END */$$
 DELIMITER ;
 
+/* Procedure structure for procedure `delete_entity_attribute_values` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `delete_entity_attribute_values` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_entity_attribute_values`(IN _entity_id INT,IN _attribute_id INT , IN _table VARCHAR(100) )
+BEGIN
+	SET @stmt = CONCAT("DELETE FROM ",_table," WHERE entity_id = ", _entity_id," AND attribute_id = ", _attribute_id );
+	PREPARE statement FROM @stmt;
+	EXECUTE statement; 
+END */$$
+DELIMITER ;
+
 /* Procedure structure for procedure `delete_entity_type_data` */
 
 /*!50003 DROP PROCEDURE IF EXISTS  `delete_entity_type_data` */;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `delete_entity_type_data`(IN _entity_type_id INT)
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_entity_type_data`(IN _entity_type_id INT)
 BEGIN
 	SET @stmt = CONCAT("DELETE 
 	  sset, sc, sc2, sc3, sc4, sc5, sc6, sc8, sc9, sc10, sc11, sc12, sc13, sc14, sc15, sc16 ,sme
@@ -170,7 +109,54 @@ BEGIN
 	    LEFT JOIN `sys_module` sme ON sme.`entity_type_id` = sset.`entity_type_id`
 	WHERE sset.`entity_type_id` = ",_entity_type_id);
 	
-	#select @stmt;
+	
+	PREPARE statement FROM @stmt;
+	EXECUTE statement;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `get_entities_listing` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `get_entities_listing` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_entities_listing`(IN _entity_type_id INT, IN _attribute_code VARCHAR(250))
+BEGIN
+	DECLARE _attribute_id INT(20);
+	DECLARE _data_type VARCHAR(250);
+	
+	SELECT attribute_id, sys_data_type.type INTO _attribute_id, _data_type FROM sys_attribute 
+LEFT JOIN sys_data_type ON sys_data_type.data_type_id = sys_attribute.data_type_id
+WHERE attribute_code = _attribute_code;
+	
+	SET @stmt = CONCAT("SELECT entity_id,value FROM sys_entity_", _data_type, " WHERE entity_type_id = ", _entity_type_id, ' AND deleted_at is NULL', " AND  attribute_id = ",_attribute_id );
+	
+	
+	PREPARE statement FROM @stmt;
+	EXECUTE statement;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `get_item_inventory` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `get_item_inventory` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_item_inventory`(IN _entity_id INT, IN _attribute_code VARCHAR(250))
+BEGIN
+	DECLARE _attribute_id INT(20);
+	DECLARE _data_type VARCHAR(250);
+	
+	SELECT attribute_id, sys_data_type.type INTO _attribute_id, _data_type FROM sys_attribute 
+LEFT JOIN sys_data_type ON sys_data_type.data_type_id = sys_attribute.data_type_id
+WHERE attribute_code = _attribute_code;
+	
+	SET @stmt = CONCAT("SELECT value FROM sys_entity_", _data_type, " WHERE entity_id = ", _entity_id, 
+	" AND  attribute_id = ",_attribute_id);
+	
+	
 	PREPARE statement FROM @stmt;
 	EXECUTE statement;
     END */$$
@@ -182,7 +168,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `hard_delete_entity_data`( IN _identifier VARCHAR(100))
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `hard_delete_entity_data`( IN _identifier VARCHAR(100))
 BEGIN
 	DECLARE _entity_type_id  TINYINT(3);
 	SET _entity_type_id = (SELECT entity_type_id FROM sys_entity_type WHERE identifier = CONVERT(_identifier USING utf8) COLLATE utf8_swedish_ci);
@@ -200,23 +186,41 @@ BEGIN
 END */$$
 DELIMITER ;
 
+/* Procedure structure for procedure `soft_delete_entity_data` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `soft_delete_entity_data` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `soft_delete_entity_data`( IN _entity_id INT(10), IN _deleted_at DATETIME)
+BEGIN
+	
+	UPDATE `sys_entity_int` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id;
+	UPDATE `sys_entity_varchar`  SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
+	UPDATE `sys_entity_decimal` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
+	UPDATE `sys_entity_text` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
+	UPDATE `sys_entity_time` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ;
+	UPDATE `sys_entity_datetime` SET deleted_at = _deleted_at WHERE `entity_id` = _entity_id ; 
+END */$$
+DELIMITER ;
+
 /* Procedure structure for procedure `update_category_product_count` */
 
 /*!50003 DROP PROCEDURE IF EXISTS  `update_category_product_count` */;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `update_category_product_count`(IN _category_id INT,  IN _count_operator VARCHAR(1))
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `update_category_product_count`(IN _category_id INT,  IN _count_operator VARCHAR(1))
 BEGIN
-	#WHILE _category_id > 0 DO
+	
 	
 		SET @stmt = CONCAT("UPDATE sys_category SET `product_count` = `product_count` ", _count_operator, ' ', "1"," WHERE category_id = ", _category_id);
 		SET _category_id = (SELECT parent_id FROM sys_category WHERE category_id = _category_id);
-		#select @stmt;
+		
 		PREPARE statement FROM @stmt;
 		EXECUTE statement; 
 		
-	#END WHILE;
+	
 END */$$
 DELIMITER ;
 
@@ -226,7 +230,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `update_entity_attribute_value`(IN _entity_id INT, IN _attribute_code VARCHAR(250), 
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `update_entity_attribute_value`(IN _entity_id INT, IN _attribute_code VARCHAR(250), 
 			IN _attribute_value INT)
 BEGIN
 	DECLARE _attribute_id INT(20);
@@ -238,7 +242,7 @@ WHERE attribute_code = _attribute_code;
 	SET @stmt = CONCAT("UPDATE sys_entity_", _data_type, " SET `value` = ", _attribute_value, 
 	" WHERE entity_id = ", _entity_id, " AND  attribute_id = ",_attribute_id);
 	
-	#select @stmt;
+	
 	PREPARE statement FROM @stmt;
 	EXECUTE statement; 
     END */$$
@@ -250,7 +254,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `update_item_inventory`(IN _entity_id INT, IN _attribute_code VARCHAR(250), 
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `update_item_inventory`(IN _entity_id INT, IN _attribute_code VARCHAR(250), 
 			IN _attribute_value INT, IN _attribute_operator VARCHAR(1))
 BEGIN
 	DECLARE _attribute_id INT(20);
@@ -262,7 +266,7 @@ WHERE attribute_code = _attribute_code;
 	SET @stmt = CONCAT("UPDATE sys_entity_", _data_type, " SET `value` = `value` ", _attribute_operator, ' ', _attribute_value, 
 	" WHERE entity_id = ", _entity_id, " AND  attribute_id = ",_attribute_id);
 	
-	#select @stmt;
+	
 	PREPARE statement FROM @stmt;
 	EXECUTE statement; 
     END */$$
@@ -274,7 +278,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE PROCEDURE `update_product_count_in_category`()
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `update_product_count_in_category`()
 BEGIN
 	SET @stmt = CONCAT("UPDATE 
 	  `sys_category` sc 
