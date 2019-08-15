05e3eaf94f7d5d2779bd2b9f5e6212fac4d25eb6
samreen <samreen.quyyum@cubixlabs.com>
2019-08-15 17:40:36 +0500

    Secondry header update

--- resources/views/web/main.blade.php
+++ resources/views/web/main.blade.php
@@ -55,37 +55,7 @@
             </div>
         </div>
 
-        <div class="container pageNavWrap">
-            <div class="greedy-overflow"></div>
-            <div class="greedy-nav page-nav" id="animationHover">
-                <button type="button" class="pull-right">More</button>
-                <ul class=" menus visible-links">
-                    <div style="
-														position: absolute;
-														top: 50%;
-														left: 50%;
-														margin-top: -50px;
-														margin-left: -50px;
-														width: 100px;
-														height: 100px;
-													"
-													id="LoadingImage" align="center" style="display: none">
-											  <div class="floatingCirclesG">
-												<div class="f_circleG frotateG_01"></div>
-												<div class="f_circleG frotateG_02"></div>
-												<div class="f_circleG frotateG_03"></div>
-												<div class="f_circleG frotateG_04"></div>
-												<div class="f_circleG frotateG_05"></div>
-												<div class="f_circleG frotateG_06"></div>
-												<div class="f_circleG frotateG_07"></div>
-												<div class="f_circleG frotateG_08"></div>
-											</div>
-
-										</div>
-						</ul>
-						<ul class='hidden-links hidden'></ul>
-					</div>
-				</div>
+        @include("web/includes/secondry_header")
 				<div class="main-banner">
 					<div class="container">
 						<div class="row">
