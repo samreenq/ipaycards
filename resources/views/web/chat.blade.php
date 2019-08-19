a1e8eeb74c6f401f42c3050cc68fada8cd85f24e
samreen <samreen.quyyum@cubixlabs.com>
2019-08-19 16:47:10 +0500

    Zendesk chat

---
+++ resources/views/web/chat.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://localhost/ipay-cards/public/web/img/favicon.png">
+
+    <!-- Localhost -->
+    <meta name="google-signin-client_id" content="812606195787-0b51kpjo3m57ehqge2eeq29ftsmivv4j.apps.googleusercontent.com">
+
+    <title>iPayCards</title>
+</head>
+<body>
+<!-- Start of salmancubix Zendesk Widget script -->
+<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=cb6b19b8-fca9-402f-8523-79d21b509697"> </script>
+<!-- End of salmancubix Zendesk Widget script -->
+</body>
+</html>
