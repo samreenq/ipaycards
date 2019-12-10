fd62070e9f8538c8438737d8a9a7f88abdb826a2
zubair ahmed <zubair.ahmed@cubixlabs.com>
2019-12-10 18:49:59 +0500

    Updated Cart Design

--- resources/views/web/includes/product/cart_list.blade.php
+++ resources/views/web/includes/product/cart_list.blade.php
@@ -14,67 +14,41 @@ foreach ( $products  as $productItemsList )
 {
 	if(isset($productItemsList['entity_id']) && !empty($productItemsList['entity_id'])){
 ?>
-				<tr> 
-					<td colspan="2"><img src="<?php echo $productItemsList['thumb'];?>" width="63"/></td>
-					<td>
-							<table width="100%">
-								<tr>
-										<td class="addItemRecipe">{{ $productItemsList['title'] }}</td>
-										{{--<td class="addItemWeight" align="right">{!! $productItemsList['product_code'] !!}--}}
-										</td>
-										<td>
-											<a class="cross_wrap" href="javascript:void(0)" >
-												<span style="font-size:75%;color:#000;margin-left: 50%;margin-bottom: 57%;" onclick="deleteCartProduct('@if(isset($productItemsList['product_code'])){{$productItemsList['product_code']}}@endif','{{ route('add_to_cart') }}','{{ route('show_cart') }}','{{route('total_price')}}')" > <!--class="clancelItem" --> 
-													<span class="icon-tt-close-icon"></span>
-												</span>					  									
-											</a>
-										</td>
-								</tr>
-								<tr >
-									<div class="row2" >
-										<td class="addItemRate">
-											<div class="left"><?php echo $currency; ?>  </div>
-											<div class="right price{{ $n }}" > @if( isset($productItemsList['price']) && isset($productItemsList['product_quantity'])){{$productItemsList['price']*$productItemsList['product_quantity']}} @endif</div>
-										</td>
-										<td class="addItemMore" align="center">
-											
-											<div class="count-input space-bottom">		
-										
-												<a class="incr-btn incr-btn1  mrn text-right" data-action="decrease" href="javascript:void(0)">
-													<span class="icon-tt-minus-icon"></span>
-												</a>
-												
-												<input class="product_code" type="hidden" name="code" value="@if( isset($productItemsList['product_code']) ){{$productItemsList['product_code']}}@endif"/>
-												<input class="price_tmp" type="hidden" name="code" value="@if( isset($productItemsList['price']) && isset($productItemsList['product_quantity']) ) {{ $productItemsList['price'] * $productItemsList['product_quantity'] }} @endif"/>
-												
-												<input class="price_id" type="hidden" name="code" value="{{ $n }}"/>
-												<input class="quantity{{ $n }}" type="number" readonly name="quantity" value="@if(isset($productItemsList['product_quantity'])){{$productItemsList['product_quantity']}}@endif"/>
-											
-											
-											
-												<a class="incr-btn incr-btn1 mln text-left" data-action="increase"  href="javascript:void(0)">
-													<span class="icon-tt-plus-icon"></span>
-												</a>
 
-											</div>
-
-										</td>
+<tr>
+	<td><img class="basketListImage" src="<?php echo $productItemsList['thumb'];?>" width="63"/></td>
+	<td>
+		<div class="addItemRecipe addItemRecipeTitle">{{ $productItemsList['title'] }}</div>
+		<div class="addItemRate addItemRateAed">
+			<div class="left"><?php echo $currency; ?>  </div>
+			<div class="right price{{ $n }}" > @if( isset($productItemsList['price']) && isset($productItemsList['product_quantity'])){{$productItemsList['price']*$productItemsList['product_quantity']}} @endif</div>
+		</div>
+		<div class="aedEach" style="display: block;font-weight: 400;color: #c2c5d1;font-size: 13px;"> <?php echo $currency; ?> @if(isset($productItemsList['price'])){{ $productItemsList['price']}}@endif each</div>
+	</td>
+	<td>
+		<a class="cross_wrap reciptClose" href="javascript:void(0)" >
+			<span style="font-size:75%;color:#000;margin-left: 50%;margin-bottom: 57%;" onclick="deleteCartProduct('@if(isset($productItemsList['product_code'])){{$productItemsList['product_code']}}@endif','{{ route('add_to_cart') }}','{{ route('show_cart') }}','{{route('total_price')}}')" > <!--class="clancelItem" -->
+				<span class="icon-tt-close-icon"></span>
+			</span>
+		</a>
+		<div class="addItemMore" align="center">
+			<div class="count-input space-bottom ipayCountInput">
+				<a class="incr-btn incr-btn1  mrn text-right" data-action="decrease" href="javascript:void(0)">
+					<span class="icon-tt-minus-icon"></span>
+				</a>
+				<input class="product_code" type="hidden" name="code" value="@if( isset($productItemsList['product_code']) ){{$productItemsList['product_code']}}@endif"/>
+				<input class="price_tmp" type="hidden" name="code" value="@if( isset($productItemsList['price']) && isset($productItemsList['product_quantity']) ) {{ $productItemsList['price'] * $productItemsList['product_quantity'] }} @endif"/>
+				<input class="price_id" type="hidden" name="code" value="{{ $n }}"/>
+				<input class="quantity{{ $n }}" type="number" readonly name="quantity" value="@if(isset($productItemsList['product_quantity'])){{$productItemsList['product_quantity']}}@endif"/>
+				<a class="incr-btn incr-btn1 mln text-left" data-action="increase"  href="javascript:void(0)">
+					<span class="icon-tt-plus-icon"></span>
+				</a>
+			</div>
+		</div>
+	</td>
+</tr>
 
 
-									</div>
-
-								</tr>
-							</table>
-
-
-							<span style="display: block;font-weight: 400;color: #c2c5d1;font-size: 13px;"> <?php echo $currency; ?> @if(isset($productItemsList['price'])){{ $productItemsList['price']}}@endif each</span>
-
-
-
-
-					</td>
-
-				</tr>
 
 				<tr class="borderB"><td colspan="3"><div></div></td></tr>
 <?php 
--- resources/views/web/includes/product/wish_list.blade.php
+++ resources/views/web/includes/product/wish_list.blade.php
@@ -12,47 +12,30 @@ if(isset($wishlist))
 		foreach ( $wishlist  as $wishlistItems )
 		{
 ?>
-			
-			<tr> 
-				<td colspan="2"><img src='<?php echo $wishlistItems['thumb']; ?>' width="63px" height="63px"/></td>
-				<td>
-						<table width="100%" Class="wishlistWrapper" id="wishlist-{!! $wishlistItems["entity_id"] !!}">
-							<input type="hidden" name="entity_id" id="entity_id" value="{!! $wishlistItems["entity_id"] !!}" />
-							<input type="hidden" name="title" id="title" value="{!! $wishlistItems["title"] !!}" />
-							<input type="hidden" name="product_code" id="product_code" value="{!! $wishlistItems["product_code"] !!}" />
-							<input type="hidden" name="price" id="price" value="{!! $wishlistItems["price"] !!}" />
-							<input type="hidden" name="thumb" id="thumb" value="<?php echo $wishlistItems["thumb"] ?>" />
-							<input type="hidden" name="item_type" id="item_type" value="<?php echo isset($wishlistItems["item_type"]['value']) ? $wishlistItems["item_type"]['value'] : $wishlistItems["item_type"] ?>" />
-								<tr>
-									<td class="addItemRecipe"><?php echo $wishlistItems['title'];?></td>
-									{{--<td class="addItemWeight" align="right">--}}
-											<?php 
-												// if(isset( $wishlistItems["product_code"])  && isset( $wishlistItems["product_code"]))
-												//	echo $wishlistItems["product_code"];
-											?>
-									{{--</td>--}}
-                                    <td>
-                                        <a href="javascript:void(0);" class="wishlist-cart" data-wishlist-id="{!! $wishlistItems["wishlist_entity_id"] !!}" data-id="{!! $wishlistItems["entity_id"] !!}">
-                                            <button class="addtocart wishListButton">
-                                                <span class="icon-tt-cart-Icon"></span>
-                                            </button>
-                                        </a>
-                                    </td>
-									<td class="addItemMore" align="center">
-										<a class="cross_wrap" href="#" >
-											<span style="font-size:75%;color:#000;margin-left: 50%;margin-bottom: 57%;" onclick="deleteWishlistProduct('{{ $wishlistItems['entity_id'] }}','{{ route('add_to_cart') }}','{{ route('show_cart') }}','{{route('total_price')}}','{{route('add_to_wishlist')}}','{{route('delete_to_wishlist')}}' )" > <!--class="clancelItem" -->
-												<span class="icon-tt-close-icon"></span>
-											</span>
-										</a>
-									</td>
-								</tr>
-								<tr>
-									<td class="addItemRate"><?php echo $currency; ?>  <?php if(isset( $wishlistItems["price"])) echo $wishlistItems["price"];   ?></td>
-								</tr>
-								
-						</table>
-				</td>
-			</tr>
+
+
+<tr>
+	<td>
+		<img src='<?php echo $wishlistItems['thumb']; ?>' width="63px" height="63px"/>
+	</td>
+	<td class="setWidthRow">
+		<div class="addItemRecipe addItemRecipeWishList"><?php echo $wishlistItems['title'];?></div>
+		<div class="addItemRate addItemRateWishList"><?php echo $currency; ?>  <?php if(isset( $wishlistItems["price"])) echo $wishlistItems["price"];   ?></div>
+	</td>
+	<td class="wishListButtons">
+		<a href="javascript:void(0);" class="wishlist-cart wishListCartIcon" data-wishlist-id="{!! $wishlistItems["wishlist_entity_id"] !!}" data-id="{!! $wishlistItems["entity_id"] !!}">
+			<button class="addtocart wishListButton">
+				<span class="icon-tt-cart-Icon"></span>
+			</button>
+		</a>
+		<a class="cross_wrap wishListCrossIcon" href="#" >
+			<span style="font-size:75%;color:#000;margin-left: 50%;margin-bottom: 57%;" onclick="deleteWishlistProduct('{{ $wishlistItems['entity_id'] }}','{{ route('add_to_cart') }}','{{ route('show_cart') }}','{{route('total_price')}}','{{route('add_to_wishlist')}}','{{route('delete_to_wishlist')}}' )" > <!--class="clancelItem" -->
+				<span class="icon-tt-close-icon"></span>
+			</span>
+		</a>
+	</td>
+</tr>
+
 			<tr class="borderB"><td colspan="3"><div></div></td></tr>
 			
 
